c9fcd7db555d32062b9e43f8704d8e4518e6f0cf
nc1BitW <e57520687f975cf6634c45f65c2b7e20@bitwanga.com>
2021-01-05 21:08:20 +0100

    sdk feature added (begin)

--- gen.php
+++ gen.php
@@ -8,6 +8,8 @@ class Html {
 
     public static string $method = '';
     public static string $methodLinks = '';
+    public static string $sdk = '';
+    public static string $sdkLinks = '';
     public static string $type = '';
     public static string $typeLinks = '';
     public static string $dir = 'rendered';
@@ -66,43 +68,82 @@ class Html {
       $html .= '</p>';
 
       return $html;
-    }
+    }    
+    public static function renderedMethod(stdClass $conf):bool {
 
+      $html = '';
+       
+      foreach(Method::$list as $m) {
+
+          $html .= '<h3><a name="'.$m->name.'"></a>'.$m->name.'</h3>';
+          $html .= '<p>'.$m->description.'</p>';
+          $html .= '<h4>Request</h4>';
+          $html .= '<code><pre class="prettyprint">'.json_encode($m->request, JSON_PRETTY_PRINT).'</pre></code>';
+          $html .= '<h4>ParamList types:</h4>';
+          $html .= Html::renderedParamTypeList($m->paramTypeList, $conf);
+          $html .= '<h4>Response</h4>';
+          $html .= '<code><pre class="prettyprint">'.json_encode($m->response, JSON_PRETTY_PRINT).'</pre></code>';
+          $html .= '<h4>Resource types:</h4>';
+          $html .= Html::renderedParamTypeList($m->responseTypeList, $conf);
+          Html::$methodLinks .= '<p><a href="#'.$m->name.'">'.$m->name.'</a></p>';
+      }
+      Html::$method = $html;
+
+      return true;
+    }
+    public static function renderedSdk(stdClass $conf):bool {
+
+      $html = '';  
+        
+      foreach(Sdk::$list as $s) {
+
+        $html .= '<h3><a name="'.$s->name.'"></a>'.$s->name.'</h3>';
+        $html .= '<p>'.$s->description.'</p>';
+        $html .= '<h4>Request</h4>';
+        $html .= '<code><pre class="prettyprint">'.json_encode($s->request, JSON_PRETTY_PRINT).'</pre></code>';
+        $html .= '<h4>ParamList types:</h4>';
+        $html .= Html::renderedParamTypeList($s->paramTypeList, $conf);
+        $html .= '<h4>Response</h4>';
+        $html .= '<code><pre class="prettyprint">'.json_encode($s->response, JSON_PRETTY_PRINT).'</pre></code>';
+        $html .= '<h4>Resource types:</h4>';
+        $html .= Html::renderedParamTypeList($s->responseTypeList, $conf);
+        Html::$sdkLinks .= '<p><a href="#'.$s->name.'">'.$s->name.'</a></p>';
+      }
+      Html::$sdk = $html;  
+
+      return true;
+    }
+    public static function renderedType(stdClass $conf):bool {
+
+      $html = '';  
+       
+      foreach(Type::$list as $t) {
+
+        $html .= '<h3><a name="'.$t->name.'"></a>'.$t->name.'</h3>';
+        $html .= '<p>'.$t->description.'</p>';
+        $html .= '<h4>Param list</h4>';
+        $html .= '<code><pre class="prettyprint">'.json_encode($t->paramList, JSON_PRETTY_PRINT).'</pre></code>';
+        $html .= Html::renderedTypeParamTypeList($t->paramList, $conf);
+        Html::$typeLinks .= '<p><a href="#'.$t->name.'">'.$t->name.'</a></p>';
+      }
+      Html::$type = $html;
+
+      return true;
+    }
     public static function rendered(stdClass $conf){
 
-        $html = '';
-       
-        foreach(Method::$list as $m) {
+        Html::renderedMethod($conf);
+        Html::renderedSdk($conf);
+        Html::renderedType($conf);
 
-            $html .= '<h3><a name="'.$m->name.'"></a>'.$m->name.'</h3>';
-            $html .= '<p>'.$m->description.'</p>';
-            $html .= '<h4>Request</h4>';
-            $html .= '<code><pre class="prettyprint">'.json_encode($m->request, JSON_PRETTY_PRINT).'</pre></code>';
-            $html .= '<h4>ParamList types:</h4>';
-            $html .= Html::renderedParamTypeList($m->paramTypeList, $conf);
-            $html .= '<h4>Response</h4>';
-            $html .= '<code><pre class="prettyprint">'.json_encode($m->response, JSON_PRETTY_PRINT).'</pre></code>';
-            $html .= '<h4>Resource types:</h4>';
-            $html .= Html::renderedParamTypeList($m->responseTypeList, $conf);
-            Html::$methodLinks .= '<p><a href="#'.$m->name.'">'.$m->name.'</a></p>';
-        }
-        Html::$method = $html;  
-        $html = '';      
-       
-        foreach(Type::$list as $t) {
-
-            $html .= '<h3><a name="'.$t->name.'"></a>'.$t->name.'</h3>';
-            $html .= '<p>'.$t->description.'</p>';
-            $html .= '<h4>Param list</h4>';
-            $html .= '<code><pre class="prettyprint">'.json_encode($t->paramList, JSON_PRETTY_PRINT).'</pre></code>';
-            $html .= Html::renderedTypeParamTypeList($t->paramList, $conf);
-            Html::$typeLinks .= '<p><a href="#'.$t->name.'">'.$t->name.'</a></p>';
-        }
-        Html::$type = $html;       
         $c = file_get_contents('htmlTemplate/index.html');
+
         $c = str_replace('$methodLinks', Html::$methodLinks, $c);
         $c = str_replace('$typeLinks', Html::$typeLinks, $c);
+        $c = str_replace('$sdkLinks', Html::$sdkLinks, $c);
+
         $c = str_replace('$method', Html::$method, $c);
+        $c = str_replace('$sdk', Html::$sdk, $c);
         $c = str_replace('$type', Html::$type, $c);
 
         file_put_contents(Html::$dir.'/index.html', $c);
@@ -245,6 +286,8 @@ class Type {
 
 class Method {
 
+    public static string $jsonRoot = 'rpc';
+
     public static stdClass $conf;
     public static stdClass $list;
 
@@ -273,7 +316,7 @@ class Method {
         $this->response->reason->state = "mock";
         $this->response->reason->type = $type;
     }
-    public static function confGenParamList(Method $obj, string $way, array $paramList, string $attribute, string $listName, string $alias): Method {
+    public static function confGenParamList(Method|Sdk $obj, string $way, array $paramList, string $attribute, string $listName, string $alias): Method|Sdk {
 
         $obj->$way->$attribute->$listName[0] = new stdClass();
         $element = Type::$list->$alias->paramList;
@@ -284,7 +327,7 @@ class Method {
         }
         return $obj;
     }
-    public static function confGenParam(Method $obj, string $way, array $paramList, string $attribute, string $listName, string $alias): Method {
+    public static function confGenParam(Method|Sdk $obj, string $way, array $paramList, string $attribute, string $listName, string $alias): Method|Sdk {
 
         $obj->$way->$attribute->$listName = new stdClass();
         $element = Type::$list->$alias->paramList;
@@ -322,7 +365,7 @@ class Method {
 
         foreach($conf->rpc->method as $type => $method) {
 
-            switch ($type ) {
+            switch ($type) {
                 case 'authentification':
                     $t = '';
                     break;
@@ -348,32 +391,150 @@ class Method {
 
                 foreach($def->paramList as $paramType => $attributeList) {
 
-                    $a = self::aliasSet($paramType, $conf);
-                    $func = self::funcSet($paramType);
+                    $a = Method::aliasSet($paramType, $conf);
+                    $func = Method::funcSet($paramType);
                     $obj->paramTypeList[$paramType] = $a;
                     $obj = Method::$func($obj, 'request', $attributeList, 'paramList', $paramType, $a);
                 }
                 foreach($def->reason as $paramType => $attributeList) {
                  
-                    $a = self::aliasSet($paramType, $conf);
-                    $func = self::funcSet($paramType);                    
+                    $a = Method::aliasSet($paramType, $conf);
+                    $func = Method::funcSet($paramType);                    
                     $obj->responseTypeList[$paramType] = $a;
                     $obj = Method::$func($obj, 'response', $attributeList, 'resource', $paramType, $a);
                 }
                 Method::$list->$name = $obj;
             }
         }
-        Html::confGen(get_called_class(), Method::$list);
+        Html::confGen('Method', Method::$list);
 
         return true;
     }
 }    
+
+class Sdk extends Method {
+
+  public static string $jsonRoot = 'sdk';
+
+  public static stdClass $conf;
+  public static stdClass $list;
+
+  public string $methodHash = '';
+
+  public function __construct(string $name, stdClass $def, string $type, string $description = ''){
+
+    $this->name = $name;
+    $this->description = $description;
+    $hash = new Hash($name, $def);
+    $this->methodHash = $hash->paramList->hash;
+
+    $this->request = new stdClass();
+    $this->response = new stdClass();
+  }
+  public static function confGenParamList(Method|Sdk $obj, string $way, array $paramList, string $attribute, string $listName, string $alias): Method|Sdk {
+
+      $obj->$way->$listName[0] = new stdClass();
+      $element = Type::$list->$alias->paramList;
+
+      foreach($paramList as $attributeName) {
+                                      
+          $obj->$way->$listName[0]->$attributeName = $element->$attributeName;
+      }
+      return $obj;
+  }
+  public static function confGenParam(Method|Sdk $obj, string $way, array $paramList, string $attribute, string $listName, string $alias): Method|Sdk {
+
+      $obj->$way->$listName = new stdClass();
+      $element = Type::$list->$alias->paramList;
+
+      foreach($paramList as $attributeName) {
+                                      
+          $obj->$way->$listName->$attributeName = $element->$attributeName;
+      }
+      return $obj;
+  }
+  public static function aliasSet(string $paramType, stdClass  $conf): string{
+
+      $alias = $conf->sdk->alias;
+      $a = $paramType;
+
+      if(strstr($paramType, 'Hash') !== false ) $a = 'hash';
+
+      if(isset($alias->$paramType) === true)  $a = $alias->$paramType;
+
+      $a = str_replace('List', '', $a);
+
+      return $a;
+  }
+  public static function funcSet(string $paramType): string {
+
+      $func = 'confGenParam';
+
+      if(strstr($paramType, 'List') !== false) $func = 'confGenParamList';
+
+      return $func;
+  }
+  public static function confGen(stdClass $conf):bool {
+
+      Sdk::$list = new stdClass();
+
+      foreach($conf->sdk->method as $type => $method) {
+
+          switch ($type ) {
+              case 'authentification':
+                  $t = '';
+                  break;
+              case 'key':
+                  $t = 'share|backup|lock';
+                  break;
+              case 'boarding':
+                  $t = 'default';
+                  break;
+              case 'contribution':
+                  $t = 'default';
+                  break;
+              case 'public':
+                  $t = 'default';
+                  break;
+              case 'peer':
+                  $t = 'default';
+                  break;
+          }
+          foreach($method as $name => $def) {
+
+              $obj = new Sdk($name, $def, $t);
+
+              foreach($def->paramList as $paramType => $attributeList) {
+
+                  $a = Sdk::aliasSet($paramType, $conf);
+                  $func = Sdk::funcSet($paramType);
+                  $obj->paramTypeList[$paramType] = $a;
+                  $obj = Sdk::$func($obj, 'request', $attributeList, 'paramList', $paramType, $a);
+              }
+              foreach($def->reason as $paramType => $attributeList) {
+               
+                  $a = Sdk::aliasSet($paramType, $conf);
+                  $func = Sdk::funcSet($paramType);                    
+                  $obj->responseTypeList[$paramType] = $a;
+                  $obj = Sdk::$func($obj, 'response', $attributeList, 'resource', $paramType, $a);
+              }
+              Sdk::$list->$name = $obj;
+          }
+      }
+      Html::confGen('Sdk', Sdk::$list);
+
+      return true;
+  }
+}
+
 $conf = json_decode(file_get_contents("conf.json"));
 
 Type::confGen($conf);
 Method::confGen($conf);
+Sdk::confGen($conf);
 Html::rendered($conf);
 
 echo '<a href="'.Html::$dir.'/index.html" target="_blank">'.Html::$dir.'/index.html</a><br>';
-echo '<a href="'.Html::$dir.'/Type.json" target="_blank">'.Html::$dir.'/Type.json</a><br>';
 echo '<a href="'.Html::$dir.'/Method.json" target="_blank">'.Html::$dir.'/Method.json</a><br>';
+echo '<a href="'.Html::$dir.'/Sdk.json" target="_blank">'.Html::$dir.'/Sdk.json</a><br>';
+echo '<a href="'.Html::$dir.'/Type.json" target="_blank">'.Html::$dir.'/Type.json</a><br>';
